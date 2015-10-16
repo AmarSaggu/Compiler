43035280308be0107c3516e8678e0df857f6f3db
Amar Saggu <amar.singh.saggu@gmail.com>
2015-10-16 04:12:26 +0100

    Add variables and expressions
    Reworked the compiler, adding in new types. Integers and strings are now
    in the language, along with variable declarations.
    
    Strings don't currently serve a purpose. You can just assign variables
    to strings.
    
    You can manipulate integers, performing arithmatic on both numbers
    and variables.
    
    Program lines are no longer seperated by commas. The only type of
    expression is a variable declaration, which is started by the keyword
    "let".

---
+++ syntax.ml
@@ -0,0 +1,41 @@
+type op =
+    | Add
+    | Sub
+    | Mul
+    | Div
+
+type exp =
+    | IntExp of int
+    | VarExp of string
+    | Exp of op * exp * exp
+
+type types =
+    | Int of exp
+    | Str of string
+
+type ast =
+    | VarDecl of string * types
+    | Var of string
+
+let print_op = function
+    | Add -> "+"
+    | Sub -> "-"
+    | Mul -> "*"
+    | Div -> "/"
+
+let rec print_exp = function
+    | IntExp i -> string_of_int i
+    | VarExp v -> v
+    | Exp (op,e,f) ->
+        let e' = print_exp e in
+        let f' = print_exp f in
+        let op' = print_op op in
+        "("^e'^" "^op'^" "^f'^")"
+
+let print_type = function
+    | Int i -> print_exp i
+    | Str s -> "\"" ^ s ^ "\""
+
+let print_ast = function
+    | VarDecl (name,value) -> "var " ^ name ^ " = " ^ (print_type value)
+    | Var v -> "value of " ^ v
