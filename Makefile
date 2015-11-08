bdff7ffe8d980a344eb168d4d9d7e93010dfb998
Amar Saggu <amar.singh.saggu@gmail.com>
2015-11-08 19:18:22 +0000

    Create a foundation for benchmarking
    Added bench.ml which when given arithmetic expressions, generates
    programs to execute them in various languages. It is the first towards
    performing benchmarks to compare performance.
    
    The languages that can be generated so far are:
    Yip, C, Java, Python and OCaml

--- Makefile
+++ Makefile
@@ -5,8 +5,9 @@ SRC=src
 
 MAIN = main.native
 TEST = test.native
+BENCH = bench.native
 
-all: clean_natives $(MAIN) $(TEST)
+all: clean_natives $(MAIN) $(TEST) $(BENCH)
 
 $(MAIN): 
 	$(COMPILER) $(FLAGS) $(SRC)/$@
@@ -14,8 +15,11 @@ $(MAIN):
 $(TEST):
 	$(COMPILER) $(FLAGS) $(SRC)/$@
 
+$(BENCH):
+	$(COMPILER) $(FLAGS) $(SRC)/$@
+
 clean: clean_natives
 	rm -rf _build/
 
 clean_natives:
-	rm -f $(MAIN) $(TEST)
+	rm -f $(MAIN) $(TEST) $(BENCH)
---
+++ src/bench.ml
@@ -0,0 +1,54 @@
+module String = Core.Std.String
+module Command = Core.Std.Command
+module In_channel = Core.Std.In_channel
+module Out_channel = Core.Std.Out_channel
+
+
+type lang = Yip | C | Java | Python | OCaml;;
+
+let extension = function
+    | Yip -> ".yip"
+    | C -> ".c"
+    | Java -> ".java"
+    | Python -> ".py"
+    | OCaml -> ".ml";;
+
+let boilerplate filename x = function
+    | Yip ->
+"x = " ^ x
+    | C ->
+"int main() {
+    int x = " ^ x ^ ";
+    return x;
+}"
+
+    | Java ->
+"public class " ^ filename ^ " {
+    public static void main(String[] args) {
+        int x = " ^ x ^ ";
+        System.exit(x);
+    }
+}"
+
+    | Python ->
+"x = " ^ x ^ "
+exit(x)"
+
+    | OCaml ->
+"let x = " ^ x ^ "in
+exit x";;
+
+let lang_list = [Yip; C; Java; Python; OCaml]
+
+let run filename () =
+    In_channel.read_all filename
+    |> String.strip
+    |> (fun x -> List.map (boilerplate filename x) lang_list)
+    |> List.combine lang_list
+    |> List.iter (fun (lang, prog) -> Out_channel.write_all (filename ^ (extension lang)) prog)
+
+let () =
+    Command.basic ~summary:"Generate test data for a language"
+        Command.Spec.(empty +> anon ("filename" %: file))
+        run
+    |> Command.run
